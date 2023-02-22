0b70b5538a7b0fd755ebdaccae17c06157bd0d1e
Vaishnav Ghenge <vaishnavghenge@gmail.com>
2023-02-22 22:29:47 +0530

    added dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,7 @@ function App() {
             <Dashboard />
           </Route>
 
-          <Route exact path="/leaderboard">
-            <Leaderboard />
-          </Route>
+          
         </Switch>
       </Router>
     </div>
