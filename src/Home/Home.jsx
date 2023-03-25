049c5b8d36411b4ac9f4a9f4e7bcbde471f57444
Vaishnav Ghenge <62459983+VaishnavGhenge@users.noreply.github.com>
2023-03-25 22:19:06 +0530

    added redirect and fixed create quiz button

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,12 +2,15 @@ import { useState } from "react"
 import Navbar from "../Navbar/Navbar"
 import Login from "../Login/Login"
 import "./Home.css"
+import { Redirect } from "react-router-dom";
 
 export default function Home() {
-    const [login, setLogin] = useState(false)
+    const [login, setLogin] = useState(false);
+    const [page, setPage] = useState("/");
 
     return (
         <div>
+            {page != "/" && <Redirect to={page} />}
             <div className="overlay" style={{ 'display': login ? 'block' : 'none' }}></div>
             <Login setLogin={setLogin} display={ login ? 'block' : 'none' } />
             <Navbar setLogin={setLogin} purpose="home" />
@@ -16,7 +19,7 @@ export default function Home() {
                     Creating and conducting quiz made easy
                 </div>
                 <div className="hero-btns">
-                    <button className="btn-quiz">Create quiz</button>
+                    <button className="btn-quiz" onClick={() => setPage("/create-quiz")}>Create quiz</button>
                     <button className="btn-quiz">Join quiz</button>
                 </div>
             </div>
